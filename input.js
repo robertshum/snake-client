40a5bcbc75986ffb5fecc606adf4f3a925bf6a0a
Robert Shum <robertshum@gmail.com>
2023-09-26 18:56:29 -0700

    passed con obj to input, so we can move snake.

--- input.js
+++ input.js
@@ -1,18 +1,42 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = function(key) {
+
+  //exit
   if (key === '\u0003') {
     console.log("Player left the game");
     process.exit();
   }
 
-  console.log("key", key);
+  if (key.toLowerCase() === "w") {
+    connection.write("Move: up");
+    return;
+  }
+
+  if (key.toLowerCase() === "a") {
+    connection.write("Move: left");
+    return;
+  }
+
+  if (key.toLowerCase() === "s") {
+    connection.write("Move: down");
+    return;
+  }
+
+  if (key.toLowerCase() === "d") {
+    connection.write("Move: right");
+    return;
+  }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", (data) => handleUserInput(data)); //handles input
+  connection = conn;
   return stdin;
 };
 
--- play.js
+++ play.js
@@ -2,9 +2,12 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect(); //from the destructuring in the require stmt up top
 
-setupInput(); //from the destructuring in the require stmt up top
+//from the destructuring in the require stmt up top
+const connection = connect();
+
+ //from the destructuring in the require stmt up top
+setupInput(connection);
 
 
 
