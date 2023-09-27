40e45bd26cdbe73699630dd1dd46018f3fc72241
Robert Shum <robertshum@gmail.com>
2023-09-26 17:59:14 -0700

    added initial client and connection code

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+const { setgroups } = require("process");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    setupData(conn);
+  });
+  return conn;
+};
+
+setupData = function(conn) {
+
+  conn.on("data", (data) => {
+    console.log("data", data);
+  });
+
+  conn.write("Name: ROB");
+};
+
+module.exports = connect;
---
+++ play.js
@@ -0,0 +1,8 @@
+const connect = require("./client");
+
+const con = console.log("Connecting ...");
+
+
+//from client.js
+connect();
+
