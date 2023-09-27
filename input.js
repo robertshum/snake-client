12954d366353e3d297cdd218279cb959e953f89e
Robert Shum <robertshum@gmail.com>
2023-09-26 19:24:04 -0700

    added msgs :)

--- input.js
+++ input.js
@@ -3,6 +3,7 @@ let connection;
 
 const handleUserInput = function(key) {
 
+  connection.write("");
   //exit
   if (key === '\u0003') {
     console.log("Player left the game");
@@ -30,6 +31,24 @@ const handleUserInput = function(key) {
   }
 };
 
+const banter = ["I'm a sneaky snake",
+  "Hello snake world",
+  "You look fang-cy",
+  "It's a hit or hiss",
+  "Lighthouse Labs rocks!",
+  "~>°)~~~~~~~~~~",
+  "=)"];
+
+//25 character limit for msgs
+const startBanter = () => {
+  let i = 0;
+  setInterval(() => {
+    const msg = banter[i];
+    i = i >= banter.length - 1 ? 0 : i + 1;
+    connection.write(`Say: ${msg}`);
+  }, 4 * 1000);
+};
+
 const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -37,6 +56,7 @@ const setupInput = function(conn) {
   stdin.resume();
   stdin.on("data", (data) => handleUserInput(data)); //handles input
   connection = conn;
+  startBanter();
   return stdin;
 };
 
